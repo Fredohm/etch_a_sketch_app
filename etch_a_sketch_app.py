4d9dc690909130a2d6c46bbb06ff20d3f94bfe7a
Fredohm <fredwaucquez@hotmail.fr>
2022-01-21 10:30:05 +0100

    Implement basic moves and clear screen

--- etch_a_sketch_app.py
+++ etch_a_sketch_app.py
@@ -1,8 +1,35 @@
 from turtle import Turtle, Screen
 
 turtle = Turtle()
+turtle.speed("fastest")
 screen = Screen()
 
+
+def move_forwards():
+    turtle.forward(10)
+
+
+def move_backwards():
+    turtle.backward(10)
+
+
+def counter_clockwise():
+    turtle.left(1)
+
+
+def clockwise():
+    turtle.right(1)
+
+
+def clear_drawing():
+    turtle.reset()
+
+
 screen.listen()
+screen.onkeypress(key="w", fun=move_forwards)
+screen.onkeypress(key="s", fun=move_backwards)
+screen.onkeypress(key="a", fun=counter_clockwise)
+screen.onkeypress(key="d", fun=clockwise)
+screen.onkeypress(key="c", fun=clear_drawing)
 
 screen.exitonclick()
