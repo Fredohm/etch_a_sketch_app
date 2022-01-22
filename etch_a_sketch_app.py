d937cdbc8f65004d83956b9ba28ae4c0d130ab29
Fredohm <fredwaucquez@hotmail.fr>
2022-01-22 11:51:32 +0100

    Simple final

--- etch_a_sketch_app.py
+++ etch_a_sketch_app.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 turtle = Turtle()
 turtle.speed("fastest")
+turtle.shape("turtle")
 screen = Screen()
 
 
@@ -13,23 +14,33 @@ def move_backwards():
     turtle.backward(10)
 
 
-def counter_clockwise():
-    turtle.left(1)
+def turn_counter_clockwise():
+    turtle.left(10)
 
 
-def clockwise():
-    turtle.right(1)
+def turn_clockwise():
+    turtle.right(10)
 
 
 def clear_drawing():
     turtle.reset()
 
 
+def pen_up():
+    turtle.penup()
+
+
+def pen_down():
+    turtle.pendown()
+
+
 screen.listen()
-screen.onkeypress(key="w", fun=move_forwards)
-screen.onkeypress(key="s", fun=move_backwards)
-screen.onkeypress(key="a", fun=counter_clockwise)
-screen.onkeypress(key="d", fun=clockwise)
-screen.onkeypress(key="c", fun=clear_drawing)
+screen.onkeypress(move_forwards, "Up")
+screen.onkeypress(move_backwards, "Down")
+screen.onkeypress(turn_counter_clockwise, "Left")
+screen.onkeypress(turn_clockwise, "Right")
+screen.onkeypress(clear_drawing, "space")
+screen.onkey(pen_up, "a")
+screen.onkey(pen_down, "z")
 
 screen.exitonclick()
